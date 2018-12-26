622d084df11ff8c8753218c5954c12932d875102
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2018-12-26 07:44:25 -0600

    Upgrading to SS4

--- _config.php
+++ _config.php
@@ -1,12 +1,2 @@
 <?php
 
-/**
- * Fetches the name of the current module folder name.
- *
- * @return string
- */
-if (!defined('LIBRARIAN_DIR')) {
-    define('LIBRARIAN_DIR', ltrim(Director::makeRelative(realpath(__DIR__)), DIRECTORY_SEPARATOR));
-}
-
-LibrarianAdmin::add_extension('SubsiteMenuExtension');
